f915382935f68ff24e544fc24761f530fe9fa11c
Timeless-py <vitali.ulrich@gmx.de>
2021-05-21 16:36:26 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from pywebcopy import save_website, save_webpage
+
+save_webpage(
+    url='http://example.com/',
+    project_folder='webpage/',
+)
+
+
+save_website(
+    url='example.com/',
+    project_folder='website/',
+)
