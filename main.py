e349087ce812560544e124fac710d2d79d905878
Timeless-py <vitali.ulrich@gmx.de>
2021-05-21 16:42:01 +0200

    update

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+'''
+Copy full Website
+'''
+
 from pywebcopy import save_website, save_webpage
 
 save_webpage(
     url='http://example.com/',
     project_folder='webpage/',
 )
-
 
 save_website(
     url='example.com/',
